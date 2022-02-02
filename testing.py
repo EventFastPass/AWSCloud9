3ab8a950d0e31730eb38933939c61f5c854d3373
EC2 Default User <ec2-user@ip-172-31-7-232.ap-southeast-2.compute.internal>
2022-02-02 11:19:52 +0000

    Playing with dictionaries.

--- testing.py
+++ testing.py
@@ -1,1 +1,25 @@
-print("Hello World")
+list_A = [1,2,3,4,5]
+list_B = ["A","B","C","D","E"]
+
+list_C = zip(list_B,list_A)
+
+Dict = dict(list(list_C))
+new_list = []
+
+
+for key,value in Dict.items():
+    if value == 4:
+        print("4 is in the dictionary")
+        new_list.append((value,key))
+    elif key == "C":
+        print(f"Found key {key} in the dictionary")
+        new_list.append((value,key))
+        
+print(dict(new_list))
+
+
+
+    
+    
+    
+
